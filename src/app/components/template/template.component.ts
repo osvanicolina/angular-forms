c030e762252eed78c0fafa3f12f9d21a49a23a40
osvanicolina <osvacolijim@gmail.com>
2019-11-30 10:51:45 -0400

    Getting data from template form

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
 export class TemplateComponent{
-
+  user: Object = {
+    first_name: "Osvani",
+    last_name: "Colina",
+    email: ""
+  }
   constructor() { }
 
-  save(){
-    console.log('Submit');
+  save(form: NgForm){
+    console.log('ngForm: ',form);
+    console.log('Value', form.value);
+    console.log("User", this.user);
   }
   
 }
