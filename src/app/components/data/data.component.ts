2357ac67882d01912ff704b185851908a6740fd9
osvanicolina <osvacolijim@gmail.com>
2019-12-02 09:57:19 -0400

    Add custom validation

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -25,7 +25,7 @@ export class DataComponent{
     this.form = new FormGroup({
       'name': new FormGroup({
         'first_name': new FormControl('', [Validators.required,Validators.minLength(3)]),
-        'last_name': new FormControl('', Validators.required),
+        'last_name': new FormControl('', [Validators.required, this.noColina]),
       }),
       'email': new FormControl('', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
       'hobbies': new FormArray([
@@ -43,8 +43,18 @@ export class DataComponent{
     );
   }
 
+  noColina(control: FormControl): {[s: string]: boolean}{
+    if(control.value === "colina"){
+      return {
+        noColina: true
+      }
+    }else{
+      return null;
+    }
+  }
+
   save(){
     console.log(this.form);
-    this.form.reset();//Can be reset() and reset the values anyways*/
+    //this.form.reset();//Can be reset() and reset the values anyways*/
   }
 }
