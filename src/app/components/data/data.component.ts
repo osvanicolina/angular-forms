8ef56df4661d6c1d3eb687405c4929922bc98429
osvanicolina <osvacolijim@gmail.com>
2019-12-02 09:52:29 -0400

    Add form array

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 
 
@@ -16,7 +16,8 @@ export class DataComponent{
       first_name: 'Osvani',
       last_name: 'Colina'
     },
-    email: 'osvacolijim@gmail.com'
+    email: 'osvacolijim@gmail.com',
+    hobbies: ['Jugar', 'Ver series', 'Dormir']
   }
 
   constructor() { 
@@ -26,21 +27,24 @@ export class DataComponent{
         'first_name': new FormControl('', [Validators.required,Validators.minLength(3)]),
         'last_name': new FormControl('', Validators.required),
       }),
-      'email': new FormControl('', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+      'email': new FormControl('', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'hobbies': new FormArray([
+                  new FormControl('Correr', Validators.required)
+                ])
     });
     
 
-    this.form.setValue( this.user );
+    //this.form.setValue( this.user );
   }
   
+  addHobby(){
+    (this.form.controls['hobbies'] as FormArray).push(
+      new FormControl('', Validators.required)
+    );
+  }
+
   save(){
     console.log(this.form);
-    this.form.reset({
-      name:{
-        first_name: '',
-        last_name: ''
-      },
-      email: ''
-    });//Can be reset() and reset the values anyways
+    this.form.reset();//Can be reset() and reset the values anyways*/
   }
 }
