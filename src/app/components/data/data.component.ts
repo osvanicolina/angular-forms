1113377a9cdc0212a3929d857f220c4dc27532bd
osvanicolina <osvacolijim@gmail.com>
2019-12-02 09:14:03 -0400

    Setting form groups

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,12 +11,21 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class DataComponent{
   
   form: FormGroup;
+  usuario: Object = {
+    name: { 
+      first_name: 'Osvani',
+      last_name: 'COlina'
+    },
+    email: 'osvacolijim@gmail.com'
+  }
 
   constructor() { 
 
     this.form = new FormGroup({
-      'first_name': new FormControl('', [Validators.required,Validators.minLength(3)]),
-      'last_name': new FormControl('', Validators.required),
+      'name': new FormGroup({
+        'first_name': new FormControl('', [Validators.required,Validators.minLength(3)]),
+        'last_name': new FormControl('', Validators.required),
+      }),
       'email': new FormControl('', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
   
